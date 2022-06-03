0255b3839427ccb1ed75473abfa3f6952d80cf1f
Tomasz Nowak <tomeknew1@gmail.com>
2022-06-03 15:33:49 +0100

    Game menu added

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    printf("Start of the project!\n");
+    char option = '\0';
+    //main game loop
+    while(1){
+        printf("\nOptions:\n");
+        printf("1. Start the game!\n2. Exit\n>>> ");
+        option = getchar();
+        getchar();
+
+        switch(option){
+
+
+            case '1':
+                printf("\nLet's start the game!\n");
+                break;
+
+            case '2':
+                printf("Thank you, bye!\n");
+                return 0;
+        }
+    }
     return 0;
 }
